e131482bdf5f94f1dbfacf18f20a59f293efaf3e
Zoe Nguyen <nganguyen0703@gmail.com>
2022-10-26 12:23:15 +0800

    chore: update basic App structure in App.jsx file

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Container from "@mui/material/Container";
+import { Outlet } from "react-router-dom";
+import SiteHeader from "./components/site-header/SiteHeader";
+
+function App() {
+  return (
+    <div className="App">
+      <SiteHeader />
+      <Container>
+        <Outlet />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
